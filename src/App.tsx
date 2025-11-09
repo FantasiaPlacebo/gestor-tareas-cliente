6cf28fb21f6a316f3d55f33f57da83a2397ecacf
FantasiaPlacebo <107363708+FantasiaPlacebo@users.noreply.github.com>
2025-11-09 14:54:13 -0300

    Instalacion de Vite,React y Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-gray-800 text-green-400 p-8 m-10 rounded-lg shadow-xl text-center text-2xl font-bold">
+      ¡Hola Mundo! Confirmando que Tailwind funciona.
+    </div>
+  );
+}
+export default App;
